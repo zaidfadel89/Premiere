9b7f3fbce5cd486f105ecacf6c9b2545c3c4825e
ahmed ahmed <algandel19@gmail.com>
2019-10-19 14:32:23 -0500

    about

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+
+CREATE database top_vehicle;
+
+USE top_vehicle;
+
+CREATE TABLE cars (
+  id INT NOT NULL,
+  make VARCHAR(100) NULL,
+  model VARCHAR(100) NULL,
+  eng_dscr VARCHAR(100) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE info (
+  id INT NOT NULL,
+  photo VARCHAR(1000) NULL,
+  model VARCHAR(100) NULL,
+  drive VARCHAR(100) NULL,
+  fuelType1 VARCHAR(100) NULL,
+  VClass VARCHAR(100) NULL,
+  trany VARCHAR(100) NULL,
+  createdOn VARCHAR(100) NULL,
+  fuelCost08 INT(100) NULL,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM cars;
+select * from info;
