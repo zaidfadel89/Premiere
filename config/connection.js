bac76dfe2f456d4d46ca6956b5621a6d314d8d6a
Zaid Fadel <zaid.fadel89@gmail.com>
2019-10-21 20:57:03 -0500

    starting with login page

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require('mysql');
-
-var connection = mysql.createConnection({
-  host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  port: 3306,
-  user: 'b1w6ett0jjl0fw9s',
-  password: 'ht1pmzgfl0amzyza',
-  database: '`oqgf306b39x9ybxi`'
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'b1w6ett0jjl0fw9s',
+    password: 'ht1pmzgfl0amzyza',
+    database: 'oqgf306b39x9ybxi'
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
